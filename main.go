4faee508c9903f3f5174884e02b564a8b82991b8
Jaume Moreno <morenocantoj@gmail.com>
2025-12-14 17:43:50 +0100

    servidor http básico

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
-	fmt.Printf("Ye que pasa mundo\n")
+	puerto := ":8080"
+
+	fmt.Printf("Servidor iniciando en el puerto %s\n", puerto)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Ye que pasa mundo")
+	})
+
+	err := http.ListenAndServe(puerto, nil)
+	if err != nil {
+		fmt.Printf("Error al iniciar el servidor: %v\n", err)
+		os.Exit(1)
+	}
 
 	os.Exit(0)
 }
